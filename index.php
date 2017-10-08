b8224ba9368169cc5cc69406872bc26b05004f63
Sergey-Akmalov <akmolgt@gmail.com>
2017-10-08 14:33:24 +0300

    task 2

--- index.php
+++ index.php
@@ -8,3 +8,11 @@ $age = '27';
 print "Меня зовут: $name <br>\n";
 print "Мне $age лет <br>\n";
 print "\"!|\\/'\"\\<br>\n";
+
+// Task №2 //
+
+$allDrawings = 80;
+$feltPenDrawings = 23;
+$pencilDrawings = 40;
+$paintDrawings = $allDrawings - $feltPenDrawings - $pencilDrawings;
+print $paintDrawings."<br>\n";
