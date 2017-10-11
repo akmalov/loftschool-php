f93354a267a7a59d1a0c141b3dbdb115bd967c4b
Sergey-Akmalov <akmolgt@gmail.com>
2017-10-11 20:38:51 +0300

    update task 2

--- task-2.php
+++ task-2.php
@@ -6,4 +6,9 @@ $allDrawings = 80;
 $feltPenDrawings = 23;
 $pencilDrawings = 40;
 $paintDrawings = $allDrawings - $feltPenDrawings - $pencilDrawings;
-print $paintDrawings."<br>\n";
+
+print "На школьной выставке $allDrawings рисунков. 
+        Из них $pencilDrawings выполнены карандашами, $feltPenDrawings фломастерами, 
+        остальные красками. Сколько рисунков выполнены красками? <br>\n";
+print "Количество рисунков выполненных красками = 
+        $paintDrawings";
