40d1c6ae720ba72ff01729dd6c91dc47a15c3920
Sergey-Akmalov <akmolgt@gmail.com>
2017-10-20 18:48:46 +0300

    homework 3 task 4

--- homework-3/functions.php
+++ homework-3/functions.php
@@ -24,3 +24,16 @@ function output($data, $parent = '')
         echo "$parent: $data<br>";
     }
 }
+
+function task4()
+{
+    $curl = curl_init();
+    curl_setopt($curl, CURLOPT_URL, "https://en.wikipedia.org/w/api.php?action=query&titles=Main%20Page&prop=revisions&rvprop=content&format=json");
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+    $curlExec = curl_exec($curl);
+    curl_close($curl);
+    preg_match_all('/"pageid":\d+|"title":".*?"/', $curlExec, $matches);
+    foreach ($matches[0] as $item) {
+        echo $item, '<br>';
+    }
+}
